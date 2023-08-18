f09c1978ae77ea5b8db9e019a834497b50f4627a
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-08-18 05:17:54 -0300

    feat: Adicionado o método de listagem de contatos com paginação na Agenda

--- src/main/java/br/com/ada/Agenda.java
+++ src/main/java/br/com/ada/Agenda.java
@@ -1,28 +1,40 @@
 package br.com.ada;
-
 import br.com.ada.model.Contato;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Agenda {
     private List<Contato> contatos;
-    void adicionarContato(Contato contato){
+    public Agenda() {
+        contatos = new ArrayList<>();
+    }
+    List<Contato> listarContatos(int pagina, int tamanhoPagina) {
+        List<Contato> listagem = new ArrayList<>();
+        int primeiroRegistro = tamanhoPagina * (pagina-1);
+        if (primeiroRegistro > contatos.size() -1) {
+            return listagem;
+        }
+        int ultimoRegistro = primeiroRegistro + tamanhoPagina;
+        if (contatos.size() < ultimoRegistro) {
+            ultimoRegistro = contatos.size();
+        }
+        for (int i = primeiroRegistro; i < ultimoRegistro; i++) {
+            Contato contato = contatos.get(i);
+            listagem.add(contato);
+        }
+        return listagem;
+    }
+    void adicionarContato(Contato contato) {
         contatos.add(contato);
     }
-
-    void removerContato(Contato contato){
+    void removerContato(Contato contato) {
         contatos.remove(contato);
     }
-
-    void duplicarContato(Contato contato){
-
+    void duplicarContato(Contato contato) {
     }
-
-    List<Contato> pesquisarContato(String nome){
+    List<Contato> pesquisarContato(String nome) {
         List<Contato> encontrados = new ArrayList<>();
         for (Contato contato : contatos) {
-            if(contato.getNomeCompleto().toLowerCase().contains(nome.toLowerCase())){
+            if (contato.getNomeCompleto().toLowerCase().contains(nome.toLowerCase())) {
                 encontrados.add(contato);
             }
         }
