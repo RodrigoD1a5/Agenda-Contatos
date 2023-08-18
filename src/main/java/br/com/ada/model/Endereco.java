31ff557c3a6c9db9de93647f949741a1a8dd4248
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-08-18 04:43:58 -0300

    feat:Configurações iniciais

---
+++ src/main/java/br/com/ada/model/Endereco.java
@@ -0,0 +1,78 @@
+package br.com.ada.model;
+
+import br.com.ada.enums.TipoEndereco;
+
+public class Endereco {
+    private TipoEndereco tipoEndereco;
+    private String pais;
+    private String cep;
+    private String logradouro;
+    private String cidade;
+    private String estado;
+    private String bairro;
+    private String complemente;
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemente() {
+        return complemente;
+    }
+
+    public void setComplemente(String complemente) {
+        this.complemente = complemente;
+    }
+}
